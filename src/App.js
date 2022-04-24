1fac8df1e8d3c5f4a1c7774440765cf17c518c12
Gage Lasher <rsgage21@gmail.com>
2022-04-24 10:52:26 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import logo from './logo.svg';
+import { useState } from "react";
+import React from 'react';
 import './App.css';
 import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState("")
+  const [todos, setTodos] = useState([])
   return (
-    <div className="App">
+    < div className="App" >
       <header>
         <h1>Gage's ToDo List</h1>
       </header>
-      <Form />
-    </div>
+      <Form inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos} />
+      <TodoList todos={todos} />
+    </div >
   );
 }
 
